eaaeaf20a4a3499dee4b0121eb25e3d5c0fb73c8
gfw-breaker <lixiaoming0513@gmail.com>
2020-07-25 08:17:18 +0800

    ok

--- clear-cache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -fr /usr/local/nginx/content/cache/*
-rm -fr /usr/share/nginx/cache/*
-
-service nginx restart
-
-
--- watchdog.sh
+++ watchdog.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+source /etc/profile
+
+
+# Disk 
 used=$(/usr/bin/df | grep '/dev/vda1' | awk '{print $5}')
 
 if [ ! '100%' == $used ]; then
-	echo "disk is not full yet"
-	exit 1
+	echo -e "Disk usage: \t$used"
+else
+	rm -fr /var/log/nginx/access.log-*
+	rm -fr /var/log/nginx/error.log-*
+	rm -fr /usr/local/nginx/content/cache/*
+	rm -fr /usr/share/nginx/cache/*
+	service nginx restart
 fi
 
-rm -fr /var/log/nginx/access.log-*
-rm -fr /var/log/nginx/error.log-*
 
-/root/open-proxy/clear-cache.sh
+# Memory
+mem=$(free -m | grep Mem | awk '{ print $4 }')
 
+if [ "$mem" -gt "65" ]; then
+	echo -e "Free memory: \t$mem MB"
+else
+	echo "Reboot server..."
+	$init 6
+fi
 
