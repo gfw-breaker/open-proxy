4734583b5164db61dfd7466bc5d3bcd3e98948f2
gfw-breaker <lixiaoming0513@gmail.com>
2020-08-13 11:06:02 +0800

    ok

--- install.sh
+++ install.sh
@@ -9,13 +9,17 @@ rm -fr /etc/nginx/conf.d/*
 mkdir -p /usr/share/nginx/cache
 mkdir -p /usr/share/nginx/html
 
+source config
+
 server_ip=$(/sbin/ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
 
 if [ -z $server_ip ]; then
 	server_ip=$(/sbin/ifconfig | grep "broadcast" | awk '{print $2}')
 fi
 
-data_server_ip=$server_ip
+if [ -z $data_server_ip ]; then
+	data_server_ip=$server_ip
+fi
 
 cp common/* /etc/nginx/
 cp sites/* /etc/nginx/conf.d
@@ -28,10 +32,11 @@ done
 
 for f in $(ls /usr/share/nginx/html/*.html); do
 	sed -i "s/local_server_ip/$server_ip/g" $f
+	sed -i "s/data_server_ip/$data_server_ip/g" $f
 done
 
-page=/usr/share/nginx/html/index.html
-sed -i "s#http.*m3u8#http://$data_server_ip:8009/cn/live800/playlist.m3u8#" $page
+#page=/usr/share/nginx/html/index.html
+#sed -i "s#http.*m3u8#http://$data_server_ip:8009/cn/live800/playlist.m3u8#" $page
 
 # CentOS6
 mv /etc/init.d/kaltura-nginx /etc/init.d/nginx
